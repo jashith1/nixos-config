deff6fc91c10d17ca8e84e129e2ee5e84623345d
Jashith <jashith.r1@gmail.com>
2025-11-22 18:18:29 -0600

    install vlc

--- configuration.nix
+++ configuration.nix
@@ -70,7 +70,7 @@
     grimblast slurp wl-clipboard
 
     #nice apps 
-    kitty mullvad-vpn qbittorrent
+    kitty mullvad-vpn qbittorrent vlc
 
     #neovim plugins, LSP, etc
     tree-sitter ripgrep fd prettier stylua black shfmt python3Packages.pip lua-language-server pyright typescript-language-server yaml-language-server 
