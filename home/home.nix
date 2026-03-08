03e2a9998cabde28eb75e7218b336af95334d11a
Jashith Raghavendra <jashith.r1@gmail.com>
2026-03-08 00:47:54 -0600

    default to enabling zsh integration for install programs

--- home/home.nix
+++ home/home.nix
@@ -22,6 +22,8 @@
   #     xxx
   # '';
 
+  home.shell.enableZshIntegration = true;
+
   home.sessionVariables = {
     NIXOS_OZONE_WL = "1"; #wayland apps use ozone
     ELECTRON_OZONE_PLATFORM_HINT = "auto";
